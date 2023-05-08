8b351f121a8a1caeca83fbbe580dda64ec5f8a9b
9000HAL <gsanchezscs@gmail.com>
2023-05-08 11:30:33 -0700

    FIRST gabe fork commit: 11/30am_COMFIRM SAVE?

--- garage-ticket-machine.py
+++ garage-ticket-machine.py
@@ -9,8 +9,8 @@ class ParkingGarage():
         self.ticket -= 1
         self.parking_space -=1
         self.current_ticket['paid'] = False
-    #- This should decrease the amount of tickets available by 1
-  #This should decrease the amount of parkingSpaces available by 1
+# This should decrease the amount of tickets available by 1
+#  This should decrease the amount of parkingSpaces available by 1
 
     def pay_for_parking(self):
         payment =input("Enter the payment amount")
@@ -20,13 +20,14 @@ class ParkingGarage():
         else:
             print("Your ticket value is", self.current_ticket)
 
-    # Display an input that waits for an amount from the user and store it in a variable
- #If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
- #This should update the "currentTicket" dictionary key "paid" to True
+# Display an input that waits for an amount from the user and store it in a variable
+# If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
+# This should update the "currentTicket" dictionary key "paid" to True
 
     def leave_garage(self):
-        pass
-    #- If the ticket has been paid, display a message of "Thank You, have a nice day"
+        
+
+#- If the ticket has been paid, display a message of "Thank You, have a nice day"
 # - If the ticket has not been paid, display an input prompt for payment
 # - Once paid, display message "Thank you, have a nice day!"
 # - Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
