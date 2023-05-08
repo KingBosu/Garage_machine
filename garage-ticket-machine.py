e38d686ce2fe1c704c0de20aa05bbbee9d5283c2
KingBosu <mckaywilliamiii@gmail.com>
2023-05-08 16:08:47 -0400

    Added call functions. Checked merged code.

--- garage-ticket-machine.py
+++ garage-ticket-machine.py
@@ -9,16 +9,17 @@ class ParkingGarage():
         self.ticket -= 1
         self.parking_space -=1
         self.current_ticket['paid'] = False
+        print("Please take your ticket")
 # This should decrease the amount of tickets available by 1
 #  This should decrease the amount of parkingSpaces available by 1
 
     def pay_for_parking(self):
-        payment =input("Enter the payment amount")
-        if payment == int(0):
+        payment =int(input("Enter the payment amount"))
+        if payment == 0:
             print("Their Ticket has been paid and they have 15 min to leave")
             self.current_ticket['paid'] = True
         else:
-            print("Your ticket value is", self.current_ticket)
+            print("Your ticket value is", payment)
 
 # Display an input that waits for an amount from the user and store it in a variable
 # If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
@@ -44,8 +45,8 @@ class ParkingGarage():
                 self.ticket += 1                        # INCREASE the ticket by 1
                 self.current_ticket['paid'] = True      # UPDATE the current ticket to PAID
 #####GABE LINE BREAK TO HELP READ######
-        else:
-        print("Payment not accepted. Please try again.")
+            else:
+                print("Payment not accepted. Please try again.")
 
 
 #- If the ticket has been paid, display a message of "Thank You, have a nice day"
@@ -55,5 +56,8 @@ class ParkingGarage():
 # - Update tickets list to increase by 1 (meaning add to the tickets list)
 
 
+parking_garage = ParkingGarage()
 
-        ParkingGarage()
+parking_garage.take_ticket()
+
+parking_garage.leave_garage()
